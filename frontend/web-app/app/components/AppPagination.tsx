af5544031571b140352e05acb8b65a544b84e28c
Apozzz <bosas.arnas2000@gmail.com>
2024-02-04 00:53:09 +0300

    wip: added frontend with nextjs

---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,21 @@
+import { Pagination } from 'flowbite-react';
+import React from 'react'
+
+type Props = {
+    currentPage: number
+    pageCount: number
+    setPageNumber: (page: number) => void;
+};
+
+export default function AppPagination({ currentPage, pageCount, setPageNumber }: Props) {
+    return (
+        <Pagination
+            currentPage={currentPage}
+            onPageChange={e => setPageNumber(e)}
+            totalPages={pageCount}
+            layout='pagination'
+            showIcons={true}
+            className='text-blue-500 mb-5'
+        />
+    );
+}
