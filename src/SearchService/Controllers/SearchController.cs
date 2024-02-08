8b8b5079f4fcd2a5482dd94ce7b4998172d3a0f4
Apozzz <bosas.arnas2000@gmail.com>
2024-02-08 22:11:44 +0300

    wip: added authentication and authorization part in frontend

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -21,7 +21,8 @@ public class SearchController : ControllerBase
 
         query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "make" => query.Sort(x => x.Ascending(a => a.Make))
+                .Sort(x => x.Ascending(a => a.Model)),
             "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
             _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
         };
@@ -34,12 +35,12 @@ public class SearchController : ControllerBase
             _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow),
         };
 
-        if (!string.IsNullOrEmpty(searchParams.Seller)) 
+        if (!string.IsNullOrEmpty(searchParams.Seller))
         {
             query.Match(x => x.Seller == searchParams.Seller);
         }
 
-        if (!string.IsNullOrEmpty(searchParams.Winner)) 
+        if (!string.IsNullOrEmpty(searchParams.Winner))
         {
             query.Match(x => x.Winner == searchParams.Winner);
         }
